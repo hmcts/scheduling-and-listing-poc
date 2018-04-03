5096f06c5ebe3f508b0085a165b910ddac8eacc4
Vijaya raghava Krishna shetty <vijay@Vijayas-MacBook-Pro.local>
2018-04-03 14:16:03 +0100

    hostname changed

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -6,15 +6,14 @@ module "frontend" {
   ilbIp                = "${var.ilbIp}"
   is_frontend          = true
   subscription         = "${var.subscription}"
-  additional_host_name = "rhubarb.platform.hmcts.net"
+  additional_host_name = "snl.platform.hmcts.net"
 
   app_settings = {
     # REDIS_HOST                   = "${module.redis-cache.host_name}"
     # REDIS_PORT                   = "${module.redis-cache.redis_port}"
     # REDIS_PASSWORD               = "${module.redis-cache.access_key}"
-    RECIPE_BACKEND_URL = "http://rhubarb-recipe-backend-${var.env}.service.${data.terraform_remote_state.core_apps_compute.ase_name[0]}.internal"
+    RECIPE_BACKEND_URL = "http://snl-recipe-backend-${var.env}.service.${data.terraform_remote_state.core_apps_compute.ase_name[0]}.internal"
 
     WEBSITE_NODE_DEFAULT_VERSION = "8.8.0"
   }
 }
-
