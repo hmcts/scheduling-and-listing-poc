3f5d6eb07ad48d8497fac785f69e8dd1efc4afc5
Vijaya raghava Krishna shetty <vijay@Vijayas-MacBook-Pro.local>
2018-04-11 11:20:53 +0100

    added db config

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -15,5 +15,58 @@ module "frontend" {
     # RECIPE_BACKEND_URL = "http://snl-recipe-backend-${var.env}.service.${data.terraform_remote_state.core_apps_compute.ase_name[0]}.internal"
 
     WEBSITE_NODE_DEFAULT_VERSION = "8.8.0"
+    POSTGRES_HOST = "${module.db.host_name}"
+    POSTGRES_PORT = "${module.db.postgresql_listen_port}"
+    POSTGRES_DATABASE = "${module.db.postgresql_database}"
+    POSTGRES_USER = "${module.db.user_name}"
+    POSTGRES_PASSWORD = "${module.db.postgresql_password}"
   }
+}
+
+module "db" {
+  source = "git@github.com:contino/moj-module-postgres?ref=master"
+  product = "${local.app_full_name}-postgres-db"
+  location = "West Europe"
+  env = "${var.env}"
+  postgresql_user = "rhubarbadmin"
+}
+
+module "key_vault" {
+  source = "git@github.com:contino/moj-module-key-vault?ref=master"
+  product = "${local.app_full_name}"
+  env = "${var.env}"
+  tenant_id = "${var.tenant_id}"
+  object_id = "${var.jenkins_AAD_objectId}"
+  resource_group_name = "${module.app.resource_group_name}"
+  product_group_object_id = "5d9cd025-a293-4b97-a0e5-6f43efce02c0"
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES-USER" {
+  name = "${local.app_full_name}-POSTGRES-USER"
+  value = "${module.db.user_name}"
+  vault_uri = "${module.key_vault.key_vault_uri}"
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
+  name = "${local.app_full_name}-POSTGRES-PASS"
+  value = "${module.db.postgresql_password}"
+  vault_uri = "${module.key_vault.key_vault_uri}"
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES_HOST" {
+  name = "${local.app_full_name}-POSTGRES-HOST"
+  value = "${module.db.host_name}"
+  vault_uri = "${module.key_vault.key_vault_uri}"
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES_PORT" {
+  name = "${local.app_full_name}-POSTGRES-PORT"
+  value = "${module.db.postgresql_listen_port}"
+  vault_uri = "${module.key_vault.key_vault_uri}"
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES_DATABASE" {
+  name = "${local.app_full_name}-POSTGRES-DATABASE"
+  value = "${module.db.postgresql_database}"
+  vault_uri = "${module.key_vault.key_vault_uri}"
 }
